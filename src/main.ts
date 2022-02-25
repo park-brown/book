eccfff280fb9b053e43e78bcf5fe2cc9fbd67fd2
Park cloud <340688748@qq.com>
2022-02-25 09:52:39 +0800

    save progree

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ import { createPinia } from 'pinia'
 import VueMathjax from 'vue-mathjax-next'
 import VuePlyr from 'vue-plyr'
 import ThemeProvider from './ThemeProvider.vue'
-// import QueryProvider from './QueryProvider.vue'
 import '~/sass/_reset.scss'
 import 'vue-plyr/dist/vue-plyr.css'
 const pinia = createPinia()
-// pinia.use()
+
 const app = createApp(ThemeProvider)
 const router = createRouter({
   history: createWebHistory(),
@@ -19,6 +18,7 @@ const router = createRouter({
 app.use(router)
 app.use(pinia)
 app.use(VueMathjax)
+
 app.use(VuePlyr, {
   plyr: {
     i18n: {
